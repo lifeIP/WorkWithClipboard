4215a85871dae5478bd97df8102a7d20d66dca43
lifeIP <97748864+lifeIP@users.noreply.github.com>
2022-04-04 23:58:19 +0700

    Добавьте файлы проекта.

---
+++ WorkWithClipboard/Header.h
@@ -0,0 +1,27 @@
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string.h>
+#include <windows.h>
+#include <atlstr.h>
+#include <wchar.h>
+
+bool set_data_Clipboard(char* source) {
+	//в эту переменную нужно записать текст, который в дальнейшем поместится в буфер обмена
+	//запись текста в буфер обмена
+	if (OpenClipboard(NULL))//открываем буфер обмена
+	{
+		HGLOBAL hgBuffer;
+		char* chBuffer;
+		EmptyClipboard(); //очищаем буфер
+		hgBuffer = GlobalAlloc(GMEM_DDESHARE, strlen(source) + 1);//выделяем память
+		chBuffer = (char*)GlobalLock(hgBuffer); //блокируем память
+		strcpy(chBuffer, LPCSTR(source));
+		GlobalUnlock(hgBuffer);//разблокируем память
+		SetClipboardData(CF_TEXT, hgBuffer);//помещаем текст в буфер обмена
+		CloseClipboard(); //закрываем буфер обмена
+		return 1;
+	}
+	return 0;
+}
+
---
+++ WorkWithClipboard/root.cpp
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string.h>
+#include <windows.h>
+#include <atlstr.h>
+#include <wchar.h>
+#include "Header.h"
+
+int main() {
+	setlocale(LC_ALL, "RUS");
+	char source[50] = { "Hello world!" };
+}
